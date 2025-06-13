abb7d77b7798038773d3feca3943666c94c41a7f
Полина Садилова <sadilovaPon06@gmail.com>
2025-06-13 21:19:14 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -1,5 +1,4 @@
-#ifndef HEADER_H
-#define HEADER_H
+#pragma once
 
 #include <vector>
 #include <string>
@@ -8,33 +7,33 @@
 
 using namespace std;
 
+// Узел односвязного списка
 class ListNode {
 public:
-    string word;
+    string word; // Слово, хранящееся в узле
     ListNode* next;
-    ListNode(const string& w);
+    ListNode(const string& w); // Конструктор, принимающий слово
 };
 
+// Класс для построения цепочки слов
 class ListChain {
 private:
-    vector<string> words;
+    vector<string> words; // Вектор всех слов для построения цепочки
     vector<bool> used;
     ListNode* head;
     ListNode* tail;
 
-    char getLastChar(const string& word);
-    bool findChain(int step);
-    void clearList();
+    char getLastChar(const string& word);  // Получить последний символ слова
+    bool findChain(int step);               // Рекурсивный метод для поиска цепочки
+    void clearList();                       // Очистка связного списка
 
 public:
-    ListChain(const vector<string>& wordList);
+    ListChain(const vector<string>& wordList); // Конструктор, принимающий список слов для построения цепочки
     ~ListChain();
-    bool buildChain();
-    void saveResult(const string& filename);
+    bool buildChain(); // Построение цепочки слов
+    void saveResult(const string& filename); // Сохрание результата в файл
     ListNode* getChain() const;
 };
 
-vector<string> split(const string& input);
-void saveToFile(const string& filename, const string& input);
-
-#endif // HEADER_H
+vector<string> split(const string& input); // Разделение строки на слова
+void saveToFile(const string& filename, const string& input); // Сохрание строки в файл
